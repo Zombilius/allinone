8504d23294da6ada2214cffed3a14bed7ab4b451
Tim <tim@Zo.zombilius.com>
2023-04-22 14:14:40 +0300

    commented out the output lines

--- helm/examples/bitnami-wordpress/usethis.sh
+++ helm/examples/bitnami-wordpress/usethis.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # https://github.com/bitnami/charts/tree/main/bitnami/wordpress
 
 helm install -f myvalues.yaml mywp oci://registry-1.docker.io/bitnamicharts/wordpress
@@ -5,9 +7,9 @@ helm install -f myvalues.yaml mywp oci://registry-1.docker.io/bitnamicharts/word
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm search repo wordpress
-> NAME                   	CHART VERSION	APP VERSION	DESCRIPTION                                       
-> bitnami/wordpress      	15.4.1       	6.2.0      	WordPress is the worlds most popular blogging ...
-> ...
+#> NAME                   	CHART VERSION	APP VERSION	DESCRIPTION
+#> bitnami/wordpress      	15.4.1       	6.2.0      	WordPress is the worlds most popular blogging ...
+#> ...
 helm install -f myvalues.yaml mywp bitnami/wordpress
 # or end
 
@@ -28,19 +30,19 @@ minikube tunnel
 
 helm upgrade mywp -f myvalues.v02.yaml bitnami/wordpress
 helm history mywp
-> REVISION	UPDATED                 	STATUS    	CHART           	APP VERSION	DESCRIPTION     
-> 1       	Thu Apr 20 19:28:38 2023	superseded	wordpress-15.4.1	6.2.0      	Install complete
-> 2       	Thu Apr 20 20:19:15 2023	deployed  	wordpress-15.4.1	6.2.0      	Upgrade complete
+#> REVISION	UPDATED                 	STATUS    	CHART           	APP VERSION	DESCRIPTION
+#> 1       	Thu Apr 20 19:28:38 2023	superseded	wordpress-15.4.1	6.2.0      	Install complete
+#> 2       	Thu Apr 20 20:19:15 2023	deployed  	wordpress-15.4.1	6.2.0      	Upgrade complete
 
 helm rollback mywp 1
 helm history mywp
-> REVISION	UPDATED                 	STATUS    	CHART           	APP VERSION	DESCRIPTION     
-> 1       	Thu Apr 20 19:28:38 2023	superseded	wordpress-15.4.1	6.2.0      	Install complete
-> 2       	Thu Apr 20 20:19:15 2023	superseded	wordpress-15.4.1	6.2.0      	Upgrade complete
-> 3       	Thu Apr 20 20:20:31 2023	deployed  	wordpress-15.4.1	6.2.0      	Rollback to 1   
+#> REVISION	UPDATED                 	STATUS    	CHART           	APP VERSION	DESCRIPTION
+#> 1       	Thu Apr 20 19:28:38 2023	superseded	wordpress-15.4.1	6.2.0      	Install complete
+#> 2       	Thu Apr 20 20:19:15 2023	superseded	wordpress-15.4.1	6.2.0      	Upgrade complete
+#> 3       	Thu Apr 20 20:20:31 2023	deployed  	wordpress-15.4.1	6.2.0      	Rollback to 1   
 
 
 helm delete mywp
 # or
-helm delete --purge habr
+helm delete --purge mywp
 
